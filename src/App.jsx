64ec3dba2f4a212d6d1931ab8f73b07474214ad7
Marek Zelinka <mzelinka17@gmail.com>
2024-08-05 17:49:42 +0200

    use redux for counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-function App() {
-  return <></>
+/* eslint-disable react/prop-types */
+
+function App({ store }) {
+  return (
+    <>
+      <div>{store.getState()}</div>
+      <button
+        type="button"
+        onClick={() => store.dispatch({ type: 'INCREMENT' })}
+      >
+        plus
+      </button>
+      <button
+        type="button"
+        onClick={() => store.dispatch({ type: 'DECREMENT' })}
+      >
+        minus
+      </button>
+      <button type="button" onClick={() => store.dispatch({ type: 'ZERO' })}>
+        zero
+      </button>
+    </>
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return state + 1
+    }
+    case 'DECREMENT': {
+      return state - 1
+    }
+    case 'ZERO': {
+      return 0
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+const store = createStore(counterReducer)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const render = () =>
+  root.render(
+    <React.StrictMode>
+      <App store={store} />
+    </React.StrictMode>,
+  )
+
+render()
+store.subscribe(render)
